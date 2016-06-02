6eaab0348d9bbf0664b496f187812b0a18ce6375
AndyWang2015 <andy2472625@gmail.com>
2016-06-02 20:53:58 +0800

    andy

--- js/common.js
+++ js/common.js
@@ -1,11 +1,4 @@
-﻿(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/zh_TW/sdk.js#xfbml=1&version=v2.6&appId=1417992648226716";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
-
+﻿
 $(document).ready(function(){
 	var webData ={};
 	webData.wrp=$('.wrapper');
