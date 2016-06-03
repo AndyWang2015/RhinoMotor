817770138f0023d40b06e6f4e97cc1c5740ab173
andy <andy2472625@gmail.com>
2016-06-03 20:42:32 +0800

    andy

--- js/common.js
+++ js/common.js
@@ -10,18 +10,38 @@ $(document).ready(function(){
 	webData.wrp=$('.wrapper');
 
 	//Init
+  webData.bottomDis = $(window).height() / 10;
 
   	
   //AddListener  	
-	$('.slide_show').each(slide_showfc);  	
+  $('.menu .menua').click(function(){menuClick($(this));});
+  $('.menu_mobile .menua').click(function(){menuClick($(this));});
+	$('.slide_show').each(slide_showfc);
 	$(window).load(window_load);
 	function window_load(){
 		if(webData.wrp.hasClass('press_content')) press_content();
-    if(webData.wrp.hasClass('cars_content')) cars_content();    
+    if(webData.wrp.hasClass('cars_content')) cars_content();
+    if(webData.wrp.hasClass('cars_list')) cars_list();
+    if(webData.wrp.hasClass('press_list')) press_list();
+    if(webData.wrp.hasClass('service')) service();
 	}
 
 
 	//Event 
+  function service(){
+    window.onscroll = windowOnscroll;
+    windowOnscroll();
+  }
+  function press_list(){
+    webData.nowbrands = getUrlVars()['brands'];
+    if(!webData.nowbrands) webData.nowbrands=1;
+    $('.sec_menuin .menua').eq(webData.nowbrands*1-1).addClass('on');
+  }
+  function cars_list(){
+    webData.nowbrands = getUrlVars()['brands'];
+    if(!webData.nowbrands) webData.nowbrands=1;
+    $('.sec_menuin .menua').eq(webData.nowbrands*1-1).addClass('on');
+  }
   function cars_content(){
     if($('.slide_show').length !=0) $(".slide_show .s_pic_box").mCustomScrollbar({scrollInertia:300,scrollEasing:'linear'});
     if($('.cars_width').length !=0) $(".cars_width .item").mCustomScrollbar({scrollInertia:300,scrollEasing:'linear'});
@@ -56,10 +76,33 @@ $(document).ready(function(){
 			},300);
 		}
 	}
+  function menuClick(_o){
+    var _txt = _o.find('a').text();
+    if(_txt == 'CONTACT US') gocontact();
+  }
+  function gocontact(){
+    $('html,body').animate({scrollTop:$('.contact').offset().top},500);
+  }
+  function windowOnscroll(){
+    var _bottom = window.pageYOffset + $(window).height() - webData.bottomDis;
+    var aniDom = $('.item');
+    for(var i =0; i<aniDom.length;i++){
+      if(aniDom.eq(i).offset().top<_bottom){
+        if(!aniDom.eq(i).hasClass('on')){
+          aniDom.eq(i).addClass('on');
+        }       
+      }
+      else aniDom.eq(i).removeClass('on');
+    }    
+  }
 
-})//ready end  
 
-
+})//ready end 
+function getUrlVars(){
+  var vars=[],hash;var hashes=window.location.href.slice(window.location.href.indexOf('?')+1).split('&');
+  for(var i=0;i<hashes.length;i++){hash=hashes[i].split('=');vars.push(hash[0]);vars[hash[0]]=hash[1]}
+  return vars
+}
 
 
 
